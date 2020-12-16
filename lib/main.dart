80ce21f3db2830ff229d8f010863fc42b0bd1ba6
takudo <taku.doyao@gmail.com>
2020-12-16 11:26:01 +0900

    iOSで textfield 長押しで落ちるのを修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -35,6 +33,7 @@ class MyApp extends HookWidget {
     );
 
     final localizationsDelegates = [
+      GlobalCupertinoLocalizations.delegate,
       GlobalMaterialLocalizations.delegate,
       GlobalWidgetsLocalizations.delegate,
     ];
